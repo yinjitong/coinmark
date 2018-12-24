d5ca0f2640fdaca2fd5c21a818d25bcdbfa4b573
fineyjt <13261900961@163.com>
2018-12-24 16:49:54 +0800

    1

--- startup-test.sh
+++ startup-test.sh
@@ -1,5 +1,5 @@
-ADMIN_PID=admin_pid
-APP_PID=app_pid
+ADMIN_PID=admin_test_pid
+APP_PID=app_test_pid
 # 启动admin
 if [ -f "$ADMIN_PID" ] && kill -0 $(cat "$ADMIN_PID"); then
 	echo "=========进程在运行。。。准备杀死"
@@ -8,7 +8,7 @@ if [ -f "$ADMIN_PID" ] && kill -0 $(cat "$ADMIN_PID"); then
 	rm -rf "$ADMIN_PID"
 	echo "=========进程kill完成,准备启动==========="
 fi
-nohup java -jar -Xms200m -Xmx256m  admin/target/admin-0.0.1.jar --spring.profiles.active=prod>> logs/nohup.out 2>&1 & echo $! > $ADMIN_PID
+nohup java -jar -Xms200m -Xmx256m  admin/target/admin-0.0.1.jar --spring.profiles.active=test >> logs/nohup.out 2>&1 & echo $! > $ADMIN_PID
 echo "=========启动完成========="
 #启动app
 if [ -f "$APP_PID" ] && kill -0 $(cat "$APP_PID"); then
@@ -18,5 +18,5 @@ if [ -f "$APP_PID" ] && kill -0 $(cat "$APP_PID"); then
 	rm -rf "$APP_PID"
 	echo "=========进程kill完成,准备启动==========="
 fi
-nohup java -jar -Xms200m -Xmx256m  app/target/app-0.0.1.jar --spring.profiles.active=prod>> logs/nohup.out 2>&1 & echo $! > $APP_PID
+nohup java -jar -Xms200m -Xmx256m  app/target/app-0.0.1.jar --spring.profiles.active=test >> logs/nohup.out 2>&1 & echo $! > $APP_PID
 echo "=========启动完成========="
